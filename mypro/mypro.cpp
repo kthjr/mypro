79cc65f88aab8f589dd1c46ef7a95a51aa3d1657
Valeria Bryzgalova <bryzgalovalera@mail.ru>
2018-12-17 20:10:33 +0300

    added program body

---
+++ mypro/mypro.cpp
@@ -0,0 +1,15 @@
+// mypro.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h" 
+#include <iostream> 
+using namespace std; 
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	system("pause"); 
+
+	return 0;
+}
+
